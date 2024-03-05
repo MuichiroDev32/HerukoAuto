3af45047c79c1e3f6d9a7832d47de40bc11277d5
MuichiroDev32 <154478554+MuichiroDev32@users.noreply.github.com>
2024-03-05 14:21:33 +0800

    Update ai.js

--- scripts/commands/ai.js
+++ scripts/commands/ai.js
@@ -24,7 +24,7 @@ module.exports = {
                 box.reply(`👋 |  Hello, ${userName}! Ai Searching your "${question}" Please wait....`);
                 const response = await axios.get(`http://fi3.bot-hosting.net:20265/api/gpt?question=${encodeURIComponent(formattedQuestion)}`);
                 const aiResponse = response.data.reply;
-                box.reply(`🤖 | 𝗔𝗜 |\n━━━━━━━━━━━━━━━\n ${aiResponse}` + "\n━━━━━━━━━━━━━━━\n➤ 𝙲𝚛𝚎𝚊𝚝𝚎𝚍 𝙱𝚢: 𝙹𝚊𝚢𝚖𝚊𝚛 𝚡 𝙷𝚎𝚛𝚞𝚔𝚘");
+                box.reply(`┗━━━━━━━━━━━━━┛\n𝗔𝗜 𝗥𝗘𝗦𝗣𝗢𝗡𝗦𝗘 💬\n┏━━━━━━━━━━━━━┓\n ${aiResponse}` + "\n━━━━━━━━━━━━━━━\n➤ 𝙲𝚛𝚎𝚊𝚝𝚎𝚍 𝙱𝚢: 𝙹𝚊𝚢𝚖𝚊𝚛 𝚡 𝙷𝚎𝚛𝚞𝚔𝚘");
             } catch (error) {
                 console.error("Error fetching AI response:", error);
                 box.reply("Failed to get AI response. Please try again later.");

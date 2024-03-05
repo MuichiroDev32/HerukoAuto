50686af9fc5e242e7d2ae85acad92602a33d3d86
MuichiroDev32 <154478554+MuichiroDev32@users.noreply.github.com>
2024-03-05 17:16:08 +0800

    Update zhen.js

--- scripts/commands/zhen.js
+++ scripts/commands/zhen.js
@@ -21,10 +21,10 @@ module.exports = {
 
                 const formattedQuestion = `${userName} asked: ${question}`;
 
-                box.reply(`👋 |  Hello, ${userName}! Zhen is searching your "${question}" please wait....`);
+                box.reply(`👋 |  Hello, ${userName}! Zhen is searching your "${question}" please wait....↺`);
                 const response = await axios.get(`http://fi3.bot-hosting.net:20265/api/gpt?question=${encodeURIComponent(formattedQuestion)}`);
                 const aiResponse = response.data.reply;
-                box.reply(`┏━━━━━━━━━━━━━┓\n\n𝗭𝗛𝗘𝗡 𝗥𝗘𝗦𝗣𝗢𝗡𝗦𝗘 💬\n\n┗━━━━━━━━━━━━━┛\n ${aiResponse}` + "\n━━━━━━━━━━━━━━━\n➤ 𝙲𝚛𝚎𝚊𝚝𝚎𝚍 𝙱𝚢: 𝙹𝚊𝚢𝚖𝚊𝚛 𝚡 𝙷𝚎𝚛𝚞𝚔𝚘");
+                box.reply(`┏━━━━━━━━━━━━━┓\n➤ 𝗭𝗛𝗘𝗡 𝗥𝗘𝗦𝗣𝗢𝗡𝗦𝗘 💬\n┗━━━━━━━━━━━━━┛\n ${aiResponse}` + "\n━━━━━━━━━━━━━━━\n➤ 𝙲𝚛𝚎𝚊𝚝𝚎𝚍 𝙱𝚢: 𝙹𝚊𝚢𝚖𝚊𝚛 𝚡 𝙷𝚎𝚛𝚞𝚔𝚘");
             } catch (error) {
                 console.error("Error fetching AI response:", error);
                 box.reply("Failed to get AI response. Please try again later.");

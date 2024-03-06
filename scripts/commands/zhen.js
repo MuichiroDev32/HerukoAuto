e529f7dc5ed8013250cd52b446b640406e3fadd1
MuichiroDev32 <154478554+MuichiroDev32@users.noreply.github.com>
2024-03-06 11:10:20 +0800

    Update zhen.js

--- scripts/commands/zhen.js
+++ scripts/commands/zhen.js
@@ -24,7 +24,7 @@ module.exports = {
                 box.reply(`👋 |  Hello, ${userName}! Zhen is searching your "${question}" please wait....↺`);
                 const response = await axios.get(`https://openaikey-x20f.onrender.com/api?prompt=${encodeURIComponent(formattedQuestion)}`);
                 const aiResponse = response.data.reply;
-                box.reply(`┏━━━━━━━━━━━━━━━━┓\n➤ 𝗭𝗛𝗘𝗡 𝗥𝗘𝗦𝗣𝗢𝗡𝗦𝗘 💬\n┗━━━━━━━━━━━━━━━━┛\n ${aiResponse}` + "\n━━━━━━━━━━━━━━━\n➤ 𝙲𝚛𝚎𝚊𝚝𝚎𝚍 𝙱𝚢: 𝙹𝚊𝚢𝚖𝚊𝚛 𝚡 𝙷𝚎𝚛𝚞𝚔𝚘");
+                box.reply(`┏━━━━━━━━━━━━━━━━┓\n➤ 𝗭𝗛𝗘𝗡 𝗥𝗘𝗦𝗣𝗢𝗡𝗦𝗘 💬\n┗━━━━━━━━━━━━━━━━┛\n𝗛𝗲𝗹𝗹𝗼✍️:${userName}\n${aiResponse}` + "\n━━━━━━━━━━━━━━━\n➤ 𝙲𝚛𝚎𝚊𝚝𝚎𝚍 𝙱𝚢: 𝙹𝚊𝚢𝚖𝚊𝚛 𝚡 𝙷𝚎𝚛𝚞𝚔𝚘");
             } catch (error) {
                 console.error("Error fetching AI response:", error);
                 box.reply("Failed to get AI response. Please try again later.");

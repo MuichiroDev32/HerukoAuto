039d19c83b81c3e182c12119bf252ef32a232675
MuichiroDev32 <154478554+MuichiroDev32@users.noreply.github.com>
2024-03-06 18:12:50 +0800

    Update heru.js

--- scripts/commands/heru.js
+++ scripts/commands/heru.js
@@ -24,7 +24,7 @@ module.exports = {
                 box.reply(`👋 | Hello, ${userName}! Heru Searching your "${question}" please wait....↺`);
                 const response = await axios.get(`https://hiro-api.replit.app/ai/hercai?ask=${encodeURIComponent(formattedQuestion)}`);
                 const aiResponse = response.data.reply;
-                box.reply(`┏━━━━━━━━━━━━━━━━┓\n➤ 𝗛𝗘𝗥𝗨 𝗥𝗘𝗦𝗣𝗢𝗡𝗦𝗘 💬\n┗━━━━━━━━━━━━━━━━┛\n➤ 𝙷𝚎𝚕𝚕𝚘✍️:"${userName}"\n➤ 𝚈𝚘𝚞𝚛 𝚚𝚞𝚎𝚜𝚝𝚒𝚘𝚗 𝚒𝚜:"${question}"\n\n➤ 𝙷𝚎𝚛𝚞 𝙰𝚗𝚜𝚠𝚎𝚛:\n${aiResponse}` + "\n━━━━━━━━━━━━━━━━━\n➤ 𝙲𝚛𝚎𝚊𝚝𝚎𝚍 𝙱𝚢: 𝙹𝚊𝚢𝚖𝚊𝚛 𝚡 𝙷𝚎𝚛𝚞𝚔𝚘");
+                box.reply(`┏━━━━━━━━━━━━━━━━┓\n➤ 𝗛𝗘𝗥𝗨 𝗥𝗘𝗦𝗣𝗢𝗡𝗦𝗘 💬\n┗━━━━━━━━━━━━━━━━┛\n➤ 𝙷𝚎𝚕𝚕𝚘✍️:"${userName}"\n➤ 𝚈𝚘𝚞𝚛 𝚚𝚞𝚎𝚜𝚝𝚒𝚘𝚗 𝚒𝚜:"${question}"\n\n➤ 𝙷𝚎𝚛𝚞 𝙰𝚗𝚜𝚠𝚎𝚛:\n${aiResponse}` + "\n━━━━━━━━━━━━━━━━━\n➤ 𝙼𝚘𝚍𝚒𝚏𝚒𝚎𝚍 𝙱𝚢: 𝙹𝚊𝚢𝚖𝚊𝚛 𝚡 𝙷𝚎𝚛𝚞𝚔𝚘\n➤ 𝙾𝚠𝚗𝚎𝚛 𝚕𝚒𝚗𝚔:https://www.facebook.com/100095054572020");
             } catch (error) {
                 console.error("Error fetching AI response:", error);
                 box.reply("Failed to get AI response. Please try again later.");
